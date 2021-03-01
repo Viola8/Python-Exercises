e0f436ff426f45d773e35698a5f00c30edf93834
Viola8 <30484418+Viola8@users.noreply.github.com>
2021-02-28 16:24:14 -0800

    Delete str5.py

--- str5.py
+++
@@ -1,9 +0,0 @@
-# Create a program that asks the user to enter their name and their age.
-# Print out a message addressed to them that tells them the year that they will turn 100 years old.
-
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-
-x = 100-age
-y = 2020+x
-print("You will turn 100 years old in {} years, in {}". format(x,y))
