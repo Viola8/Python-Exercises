e5f9c44c24a0705f64688b3ea9736ff22264deda
Viola8 <30484418+Viola8@users.noreply.github.com>
2021-03-20 17:36:13 -0700

    Delete str9.py

--- str9.py
+++
@@ -1,7 +0,0 @@
-def rem_index(word,n):
-    first_part = word[:n] # last index is exclusif
-    second_part = word[n+1:] # this part of code is very similar to string permutaion
-
-    return first_part+second_part # we exclude n
-print(rem_index('NLTK',1))  # Output: NTK
-print(rem_index('NLTK',3)) # Output: NLT
